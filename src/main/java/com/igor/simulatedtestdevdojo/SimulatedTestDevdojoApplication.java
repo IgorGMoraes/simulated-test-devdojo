f5953f0831d2d24abab0ee360983bf494619c34a
Igor Moraes <igor.moraes@netcracker.com>
2020-11-25 21:52:31 -0300

    initial commit
    Co-Authored-By: Igor Moraes <igorgomesmoraes@gmail.com>

---
+++ src/main/java/com/igor/simulatedtestdevdojo/SimulatedTestDevdojoApplication.java
@@ -0,0 +1,13 @@
+package com.igor.simulatedtestdevdojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulatedTestDevdojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulatedTestDevdojoApplication.class, args);
+	}
+
+}
